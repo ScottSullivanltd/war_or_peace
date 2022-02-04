c15726f3139193f1dad3363dc63c8d7dd5ea0e74
Scott Sullivan <scottsullivanltd@gmail.com>
2022-02-03 22:19:41 -0700

    Add Player class and spec

---
+++ spec/player_spec.rb
@@ -0,0 +1,21 @@
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require 'pry'
+
+RSpec.describe Player do
+  it "creates a new player with a deck or cards" do
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    deck = Deck.new([card1, card2, card3])
+
+    player = Player.new('Clarisa', deck)
+
+    expect(player).to be_an_instance_of(Player)
+    expect(player.name).to eq("Clarisa")
+    expect(player.deck).to eq(deck)
+  end
+
+end
