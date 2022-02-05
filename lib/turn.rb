37cc15f7f0479e1e025dce9e9c27e29e2dc123bb
Scott Sullivan <scottsullivanltd@gmail.com>
2022-02-04 19:36:07 -0700

    Return that the type is basic

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,13 +4,18 @@ class Turn
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
+    @type = :basic, :war, :mutually_assured_destruction
   end
 
-  # def type
-  #   :basic = player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
-  #   :war = player1.deck.rank_of_card_at(0) = player2.deck.rank_of_card_at(0)
-  #   :mutually_assured_destruction = player1.deck.rank_of_card_at(0) = player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) = player2.deck.rank_of_card_at(2)
-  # end
+  def type
+    if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
+      :basic
+    elsif @player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)
+      :war
+    elsif (@player1.deck.rank_of_card_at(0) == @player2.deck.rank_of_card_at(0)) && (@player1.deck.rank_of_card_at(2) == @player2.deck.rank_of_card_at(2))
+      :mutually_assured_destruction
+    end
+  end
 
   # def winner
   #   if type == :basic
