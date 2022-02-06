2e97c5267962be9a92292b39233b98708d5fd4f1
Scott Sullivan <scottsullivanltd@gmail.com>
2022-02-06 16:20:28 -0700

    Create a card generator file and test it makes an instance

--- spec/card_generator_spec.rb
+++ spec/card_generator_spec.rb
@@ -1,5 +1,14 @@
 require './lib/card_generator'
+require './lib/card'
+require './lib/deck'
+require './lib/player'
+require './lib/turn'
+require 'pry'
 
 RSpec.describe CardGenerator do
-  
+  it "creates an instance of card generator" do
+    generate_card = CardGenerator.new(filename)
+
+    expect(generate_card).to be_an_instance_of(CardGenerator)
+  end
 end
